3c4d917c64c1b71c4f86af80fcc9c7c06826fab9
Sasha <sasha21100@ukr.net>
2026-03-25 00:43:21 +0200

    fix notesController.js updateNote function

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -34,11 +34,15 @@ export async function deleteNote(req, res) {
 
 export async function updateNote(req, res) {
   const noteId = req.params.noteId;
-  const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, {
-    new: true,
-  });
+  const note = await Note.findOneAndUpdate(
+    { _id: noteId },
+    req.body,
+    { returnDocument: 'after' } 
+  );
+
   if (!note) {
     throw createHttpError(404, 'Note not found');
   }
+
   res.status(200).json(note);
 }
